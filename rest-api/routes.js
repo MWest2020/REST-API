cec7a9852d286757bcb121cb73fd58b45beef922
MWest2020 <mwesterweel@hotmail.com>
2021-01-28 18:10:28 +0100

    -m finished part 1 and using for starters part 2

--- rest-api/routes.js
+++ rest-api/routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 // This array is used to keep track of user records
 // as they are created.
@@ -19,8 +20,33 @@ router.post('/users', (req, res) => {
   // Get the user from the request body.
   const user = req.body;
 
-  // Add the user to the `users` array.
-  users.push(user);
+  //STEP 1: MAKE A ERROR ARRAY - Stores possible errors and attached these to the user object in the users array. This is actually a very clever way to prevent code from running. If errors => stop, if not, continue
+  const errors = [];
+  
+  // STEP 2: WRITE A CONDITIONAL THAT validates user.name exists. IF the checked property doesn't exist, attempting to acces the property will return [undefined], which is falsy. If conditional is met it will push the string to the errors array
+  if(!user.name){
+    errors.push('Please provide a value for "name" ');
+  }
+
+  if(!user.email){
+    errors.push('Please provide a value for "email"');
+  }
+
+  if(!user.password){
+    errors.push('please provide a value for "password" ')
+  } else { 
+    user.password = bcrypt.hashSync(user.password, 10);
+  }
+
+
+  if(errors.length > 0) {
+    res.status(400).json( { errors })
+  } else {
+    // Add the user to the `users` array.
+    users.push(user);
+  };
+
+  console.log(errors);
 
   // Set the status to 201 Created and end the response.
   res.status(201).end();
